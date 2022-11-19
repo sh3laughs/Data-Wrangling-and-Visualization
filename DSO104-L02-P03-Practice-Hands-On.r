9c338af0b9c6c9cffca72264c5b5c58d1257449a
sh3laughs <113483383+sh3laughs@users.noreply.github.com>
2022-11-18 22:59:49 -0800

    Lesson 2 practice Hands-Ons

---
+++ DSO104-L02-P03-Practice-Hands-On.r
@@ -0,0 +1,36 @@
+# DSO104 - Data Wrangling and Visualization
+  # Lesson 1 - Manipulating Columns and Rows
+  # Page 3 - Practice Hands-On
+
+# Here is a dataset on household energy consumption in the U.S:
+  # https://repo.exeterlms.com/documents/V2/DataScience/Data-Wrang-Visual/energy.zip
+# Give data transformations a shot on your own in R!
+
+# load libraries
+library(dplyr)
+library(tidyr)
+library(readxl)
+
+# import data
+energy = read_excel('/Users/hannah/Library/CloudStorage/GoogleDrive-gracesnouveaux@gmail.com/My Drive/Bethel Tech/Data Science/DSO104 Data Wrangling and Visualization/Lesson 2: Data Transformations/energy.xlsx')
+View(energy)
+# dataset imported and saved as a new variable, and previewed in 
+  # separate tab
+  # 9 columns, 27 rows
+
+# transpose dataset
+energy1 = t(energy)
+View(energy1)
+# dataframe transposed, saved to new variable,  and previewed in 
+  # separate tab
+  # 27 columns, 9 rows
+
+# as.data.frame() function to convert to dataframe
+energy2 = as.data.frame(energy1)
+View(energy2)
+# matrix converted to dataframe
+
+# gsub() function to rename columns
+names(energy2) = gsub('V', 'Year', names(energy2))
+head(energy2)
+# column names updated and previewed in console
---
+++ DSO104-L02-P12-Practice-Hands-On.r
@@ -0,0 +1,52 @@
+# DSO104 - Data Wrangling and Visualization
+  # Lesson 1 - Manipulating Columns and Rows
+
+# Page 12 - Practice Hands-On
+  # For this data exercise, you will be putting your data combination 
+  # knowledge into practice! Below you’ll find links to two different 
+  # data sets about the Zika virus outbreak. The first, 
+  # “ZikaColombia” has information about the outbreak in Colombia, 
+  # while the second, “ZikaUS” has information about Zika in the U.S.
+    # https://repo.exeterlms.com/documents/V2/DataScience/Data-Wrang-Visual/ZikaColombia.zip
+    # https://repo.exeterlms.com/documents/V2/DataScience/Data-Wrang-Visual/ZikaUS.zip
+
+# import data
+ZikaColombia = read_excel('/Users/hannah/Library/CloudStorage/GoogleDrive-gracesnouveaux@gmail.com/My Drive/Bethel Tech/Data Science/DSO104 Data Wrangling and Visualization/Lesson 2: Data Transformations/ZikaColombia.xlsx')
+View(ZikaColombia)
+# dataset imported and saved as a new variable, and previewed in 
+  # separate tab
+  # 2 columns, 1,155 rows
+
+ZikaUS = read_excel('/Users/hannah/Library/CloudStorage/GoogleDrive-gracesnouveaux@gmail.com/My Drive/Bethel Tech/Data Science/DSO104 Data Wrangling and Visualization/Lesson 2: Data Transformations/ZikaUS.xlsx')
+View(ZikaUS)
+# dataset imported and saved as a new variable, and previewed in 
+# separate tab
+# 2 columns, 54 rows
+
+# load libraries
+library(dplyr)
+library(tidyr)
+
+
+# It’s your job to combine them into one dataset in R, so that these 
+  # two countries can be analyzed together.
+  # 1. Should you use the merge or append function?
+      # Merge
+      # Append
+str(ZikaColombia)
+# column names:
+  # location
+  # zika_total
+
+str(ZikaUS)
+# column names:
+  # location
+  # zika_total
+
+# B/c the column names are the same AND the there is no ID style
+  # variable, I'll append
+
+  # 2. Perform your chosen function on the dataset
+Zika = rbind(ZikaColombia, ZikaUS)
+View(Zika)
+# 2 columns, 1,209 rows
