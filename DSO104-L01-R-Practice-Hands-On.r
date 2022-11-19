04dd91e7a205fff4be0e2693c559e568b3b9daa6
sh3laughs <113483383+sh3laughs@users.noreply.github.com>
2022-11-19 12:07:25 -0800

    Lesson 1 practice Hands-Ons in R

--- DSO104-L01-R-Practice-Hands-On.r
+++ DSO104-L01-R-Practice-Hands-On.r
@@ -1,5 +1,5 @@
 # DSO104 - Data Wrangling and Visualization
-  # Lesson 1 - Manipulating Columns and Rows
+  # Lesson 1 - Manipulating Columns and Rows 
 
 # Page 13 - [Practice] Hands-On
   # load libraries
